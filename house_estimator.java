196c7a5c11b74e423e72356edc8cf0cdc473f0ae
skr790 <saikiranreddy790@gmail.com>
2019-03-20 22:35:05 +0530

    Update house_estimator.java

--- house_estimator.java
+++ house_estimator.java
@@ -1,3 +1,4 @@
+package home_calc;
 import java.util.HashMap;
 
 class home_estmation {
@@ -7,7 +8,10 @@ class home_estmation {
 
 
     public int get_cost(String standards,int area) {
-        HashMap<String, Integer> m=new HashMap();
+        //Using nested if checks every condition causes time redundant
+        // the retrival time of Hashmap is O(1)
+
+        HashMap<String, Integer> m=new HashMap(); //Storing required data in Hashmap
         m.put("standard",1200);
         m.put("above standard",1500);
         m.put("high standard",1800);
@@ -18,6 +22,8 @@ class home_estmation {
 
 
     }
+
+
 
 }
 
